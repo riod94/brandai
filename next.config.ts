894c50123db18690d82bdda5b1b6a5b2b14b7f85
Rio D <riyo.s94@gmail.com>
2026-01-07 09:30:37 +0700

    fix external packages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["@neplex/vectorizer", "sharp"],
+  serverExternalPackages: ["@neplex/vectorizer", "sharp", "onnxruntime-node", "@imgly/background-removal-node"],
   images: {
     remotePatterns: [
       {
